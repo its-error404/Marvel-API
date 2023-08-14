65bbc797d51236b9bbf978d8576608a55b23f7a3
its-error404 <karthiben16@gmail.com>
2023-08-14 13:21:14 +0530

    Comments and Unnecessary logs removed

--- script.js
+++ script.js
@@ -1,91 +1,92 @@
 $(document).ready(() => {
-    const MD5Hash = "7b41717548d1933bfd2307e79888325e";
-    const timeStamp = 1;
-    const publicKey = "6cda15d0c3c12522a4d8a70197c4103f";
+  const MD5Hash = "7b41717548d1933bfd2307e79888325e";
+  const timeStamp = 1;
+  const publicKey = "6cda15d0c3c12522a4d8a70197c4103f";
 
-    const apiAllCharacters = `https://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${MD5Hash}`;
+  const apiAllCharacters = `https://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${MD5Hash}`;
 
-    const characterGrid = $("#character-grid");
-    const searchInput = $("#search-input");
+  const characterGrid = $("#character-grid");
+  const searchInput = $("#search-input");
 
-    const clearCharacterGrid = () =>
-    {
-        characterGrid.empty()
-    }
+  const clearCharacterGrid = () => {
+    characterGrid.empty();
+  };
 
-    const fetchAllCharacters = () => {
-        clearCharacterGrid();
-        $.ajax({
-            
-            url: apiAllCharacters,
-            method: 'GET',
-            success: (data) => {
-                console.log('hi')
-                const characters = data.data.results;
+  const fetchAllCharacters = () => {
+    clearCharacterGrid();
+    $.ajax({
+      url: apiAllCharacters,
+      method: "GET",
+      success: (data) => {
+        const characters = data.data.results;
 
-                characters.forEach((character) => {
-                    console.log('hi')
-                    createCharacterCard(character);
-                });
-            },
-            error: (error) => {
-                console.log("Error while fetching the data,", error)
-            }
+        characters.forEach((character) => {
+          createCharacterCard(character);
         });
-    };
-
-    const fetchSingleCharacter = (e) => {
-        clearCharacterGrid();
-        e.preventDefault();
-        
-        const characterName = $('#characterName').val();
-        const apiSingleCharacter = `https://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${MD5Hash}&name=${encodeURIComponent(characterName)}`;
-
-        $.ajax({
-            url: apiSingleCharacter,
-            method: 'GET',
-            success: (data) => {
-                const singleCharacter = data.data.results[0]
-                    createCharacterCard(singleCharacter);
-            },
-            error: (error) => {
-                console.log("Error while fetching the data", error)
-            }
-        });
-    };
-
-    const fetchCharacterByNameStartsWith = (searchQuery) => {
-        clearCharacterGrid();
-        $.ajax({
-            url: `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${encodeURIComponent(searchQuery)}&apikey=${publicKey}&hash=${MD5Hash}&ts=${timeStamp}`,
-            method: 'GET',
-            success: (data) => {
-                console.log("hii")
-                const characters = data.data.results;
-                characters.forEach((character) => {
-                    createCharacterCard(character);
-                });
-            },
-            error: (error) => {
-                console.log("Error while fetching the data", error);
-            }
-        });
-    };
-
-    $('#search-button').on('click', () => {
-        const searchValue = searchInput.val()
-            fetchCharacterByNameStartsWith(searchValue);
+      },
+      error: (error) => {
+        console.log("Error while fetching the data,", error);
+      },
     });
+  };
 
-    $('#multiple-button').on('click', fetchAllCharacters);
+  const fetchSingleCharacter = (e) => {
+    clearCharacterGrid();
+    e.preventDefault();
 
-    $('#character-form').on('submit', (e) => {
-        fetchSingleCharacter(e);
+    const characterName = $("#characterName").val();
+    const apiSingleCharacter = `https://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${MD5Hash}&name=${encodeURIComponent(
+      characterName
+    )}`;
+
+    $.ajax({
+      url: apiSingleCharacter,
+      method: "GET",
+      success: (data) => {
+        const singleCharacter = data.data.results[0];
+        createCharacterCard(singleCharacter);
+      },
+      error: (error) => {
+        console.log("Error while fetching the data", error);
+      },
     });
+  };
 
-    const createCharacterCard = (character) => {
-        const characterCard = $("<div>").addClass("card").html(
-            `
+  const fetchCharacterByNameStartsWith = (searchQuery) => {
+    clearCharacterGrid();
+    $.ajax({
+      url: `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${encodeURIComponent(
+        searchQuery
+      )}&apikey=${publicKey}&hash=${MD5Hash}&ts=${timeStamp}`,
+      method: "GET",
+      success: (data) => {
+        const characters = data.data.results;
+        characters.forEach((character) => {
+          createCharacterCard(character);
+        });
+      },
+      error: (error) => {
+        console.log("Error while fetching the data", error);
+      },
+    });
+  };
+
+  $("#search-button").on("click", () => {
+    const searchValue = searchInput.val();
+    fetchCharacterByNameStartsWith(searchValue);
+  });
+
+  $("#multiple-button").on("click", fetchAllCharacters);
+
+  $("#character-form").on("submit", (e) => {
+    fetchSingleCharacter(e);
+  });
+
+  const createCharacterCard = (character) => {
+    const characterCard = $("<div>")
+      .addClass("card")
+      .html(
+        `
             <div class="image">
                 <img src="${character.thumbnail.path}.${character.thumbnail.extension}">
             </div>
@@ -96,7 +97,7 @@ $(document).ready(() => {
                 <b>Resource URL:</b>&nbsp;<a  href="${character.urls[0].url}">Learn More</a>
             </div>
             `
-        );
-        characterGrid.append(characterCard);
-    };
+      );
+    characterGrid.append(characterCard);
+  };
 });
